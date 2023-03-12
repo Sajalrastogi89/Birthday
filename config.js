baa3cd7d402d61715aca6af929bcfe52bf65762a
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2023-03-12 19:19:51 +0530

    updated

--- config.js
+++ config.js
@@ -1,16 +1,21 @@
 var config = {
     texts: [
-        "Here's to you, my beloved cutie pie",
-        "Today is your birthday",
-        "This is our third birthday together",
-        "Do you remember we celebrated last year at Wang Po hot pot?",
-        "This year, let's indulge in even more delicious food",
-        "I'll make sure to feed my adorable little piggy until you're satisfied",
-        "And then take you away in my arms",
-        "Yummy~~"
+        "Happy birthday to my wonderful friend Akshita!", "My dear friend",
+        "On this special day, I want to wish you all the happiness in the world",
+        "You are an incredible person with a kind heart and I feel grateful to have you in my life",
+        "As you pursue your dreams of becoming a doctor",
+        " I have no doubt that you will achieve your goals with your determination and hard work",
+        "Your singing talent is another testament to your creativity and passion",
+        "Akshita, I have been thinking of you today",
+        "and I truly hope that a miracle happens and my phone rings with your call",
+        "I miss our conversations and catching up with you would be like revisiting our earlier conversations.",
+        "It would be wonderful to talk with you on your special day",
+        "Once again, happy birthday my dear friend!",
+        "May all your dreams come true and may this year bring you endless joy and success. Enjoy your day to the fullest!",
+        "Akshita~~"
     ],
     imgs: {
-        "My beloved cutie pie": "./imgs/xiaokeai.png",
+        "My dear friend": "./imgs/xiaokeai.png",
     },
     desc: {
         turn_on: "Begin",
